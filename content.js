6f5a9a98e43979de3c0eb9381d5d2974ecb46bb1
kayantchian <kayan.tchian@cardapioweb.com>
2023-10-02 13:55:38 -0300

    manifest criado, content.js e poup.js em producao

--- content.js
+++ content.js
@@ -1,23 +1,54 @@
-function clickProductElements() {
-    const productElements = document.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small[data-v-58044642]');
-    
-    productElements.forEach((productElement) => {
-      try {
-        productElement.click();
-        console.log(productElement.textContent);
-      } catch (error) {
-        if (error instanceof DOMException && error.name === 'NotAllowedError') {
-          console.log(`Clicar no elemento não é permitido: ${productElement.textContent}`);
-        } else {
-          console.error(error);
-        }
-      }
-    });
-  }
-  
-  // Escute mensagens da extensão
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'clickProductElements') {
-      clickProductElements();
-    }
+// Função para clicar em category-card, acessar páginas individuais e extrair dados
+function scrapeData() {
+  const scrapeData = [];
+
+  // Seletor para encontrar todos os elementos category-card
+  let categoryCards = document.querySelectorAll('div.category-card');
+
+  // Iterar sobre os elementos category-card
+  categoryCards.forEach((categoryCard, categoryIndex) => {
+    // Clique no category-card para acessar a página individual
+    categoryCard.click();
+
+    // Aguarde um período de tempo para garantir que a página individual seja carregada
+    setTimeout(() => {
+      // Seletor para encontrar todos os elementos product-card
+      let productCards = document.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small[data-v-58044642]');
+
+      // Iterar sobre os elementos product-card
+      productCards.forEach((productCard, productIndex) => {
+        // Clique no product-card para acessar a página do produto
+        productCard.click();
+
+        // Aguarde um período de tempo para garantir que a página do produto seja carregada
+        setTimeout(() => {
+          // Seletor para encontrar e extrair os dados da página do produto
+          let priceNow = document.querySelector('span.price__now.font-3').textContent;
+          let imgSrc = document.querySelector('img').src;
+          let descricao = document.querySelector('span.weight-400').textContent;
+
+          // Crie um objeto com os dados do produto
+          let productData = {
+            priceNow,
+            imgSrc,
+            descricao,
+          };
+
+          // Adicione o objeto ao array de dados extraídos
+          scrapedData.push(productData);
+
+          window.history.back();
+          // Volte para a página anterior (a página da categoria) antes de continuar
+        }, 3000); // Espere 3 segundos antes de extrair dados da página do produto (ajuste conforme necessário)
+      });
+    }, 3000); // Espere 3 segundos antes de extrair dados da página individual (ajuste conforme necessário)
   });
+  // Retorne os dados extraídos como um JSON
+  return JSON.stringify(scrapedData);
+}
+
+// Execute a função quando a página estiver totalmente carregada
+window.addEventListener('load', () => {
+  const jsonData = scrapeData();
+  console.log(jsonData); // Exibe o JSON no console
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,28 @@
-// popup.js
-
-document.addEventListener('DOMContentLoaded', function () {
-  const scrapeButton = document.getElementById('scrapeButton');
-
-  scrapeButton.addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'clickProductElements' });
+// Função para injetar o script na página atual
+async function injectScript(tabId) {
+  try {
+    // Injeta o script na página atual
+    const [tab] = await chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: scrapeData
     });
+
+    // Exibe o resultado na página do popup
+    const jsonDisplay = document.getElementById('json-display');
+    const jsonData = JSON.stringify(tab.result, null, 2); // Converte o objeto em uma string JSON formatada
+    jsonDisplay.textContent = jsonData;
+  } catch (error) {
+    console.error('Erro ao injetar o script:', error);
+  }
+}
+
+// Função para lidar com o clique no botão
+document.getElementById('scrape-button').addEventListener('click', () => {
+  // Obtém a guia ativa
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) {
+      // Chama a função para injetar o script na guia ativa
+      injectScript(tabs[0].id);
+    }
   });
 });
