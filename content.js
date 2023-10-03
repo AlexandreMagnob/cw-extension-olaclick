230b827a6be998c585552aa4483f52d345782c1c
kayantchian <kayan.tchian@cardapioweb.com>
2023-10-03 16:46:10 -0300

    windows back history deve ser atualizado, mas os dados estao sendo capturados

--- content.js
+++ content.js
@@ -1,108 +1,118 @@
-// Função para clicar em category-card, acessar páginas individuais e extrair dados
-function scrapeData() {
-  const scrapedData = [];
+class Scrapy {
+  constructor() {
+    this.scrapedData = {}; // Alterado para um objeto (dicionário)
+    this.complementsDict = {};
+    this.optionsComplement = {};
+  }
 
-  // Seletor para encontrar todos os elementos category-card
-  let categoryCards = document.querySelectorAll('div.category-card');
-  console.log("Tamanho das categorias" + categoryCards.length);
+  checkAndScrape() {
+    const categoryCards = document.querySelectorAll('div.category-card');
+    
+    if (categoryCards.length > 0) {
+      this.clickCategoryCards(); 
+    } else {
+      this.clickProductCards();
+    }
+  }
 
-  // Iterar sobre os elementos category-card
-  categoryCards.forEach((categoryCard) => {
-    // Clique no category-card para acessar a página individual
-    categoryCard.click();
+  clickCategoryCards() {
+    let categoryCards = document.querySelectorAll('div.category-card');
+    console.log("Tamanho das categorias" + categoryCards.length);
 
-    // Aguarde um período de tempo para garantir que a página individual seja carregada
-    setTimeout(() => {
-      // Seletor para encontrar todos os elementos product-card
-      let productCards = document.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small[data-v-58044642]');
-      console.log("Tamanho dos produtos" + productCards.length);
+    categoryCards.forEach((categoryCard) => {
+      categoryCard.click();
+      this.clickProductCards();
+      window.history.go(-1);
+    });
+  }
 
-      // Iterar sobre os elementos product-card
-      productCards.forEach((productCard) => {
-        let innerDiv = productCard.querySelector('div.item-card-container.row.justify-between');
-        if (innerDiv) {
-          innerDiv.click();
-        }
-        // Aguarde um período de tempo para garantir que a página do produto seja carregada
-        setTimeout(() => {
-          // Seletor para encontrar e extrair os dados da página do produto
-          let titleElement = document.querySelector('span.font-5');
-          let priceNowElement = document.querySelector('span.price__now.font-3');
-          let imgElement = document.querySelector('img');
-          let descricaoElement = document.querySelector('span.weight-400');
+  clickProductCards() {
+    let categoryDivs = document.querySelectorAll('.category-container[data-v-c24acdb4]');
 
-          // Verifique se os elementos foram encontrados antes de acessar suas propriedades
-          let title = titleElement ? titleElement.textContent : "";
-          let priceNow = priceNowElement ? priceNowElement.textContent : "";
-          let imgSrc = imgElement ? imgElement.src : "";
-          let descricao = descricaoElement ? descricaoElement.textContent : "";
+    categoryDivs.forEach((categoryDiv) => {
+      let categoryTitleElement = categoryDiv.querySelector('span[data-v-c24acdb4]');
+      let categoryTitle = categoryTitleElement ? categoryTitleElement.textContent : "";
 
-          complementExpandables = div.querySelectorAll('div.expandable');
-          complementExpandables.forEach((complementExpandable) => {
-            // Localização de cada elemento
-            let complementElements = complementExpandable.querySelectorAll('div.expandable__fixed.py-2.px-4.pointer.bg-grey-12');
+      setTimeout(() => {
+        let productCards = document.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small[data-v-58044642]');
+        console.log("Tamanho dos produtos" + productCards.length);
 
-            complementElements.forEach((complementElement) => {
-              let complementsDict = {}; // Dicionário para armazenar as opções referentes ao complemento atual
-                
-              // Encontre o próximo elemento span com a classe apropriada para obter informações adicionais
-              let complementTitleElement = complementElement.querySelector('.expandable__fixed__header__text__title.font-2.text-black');
-              let typeComplementElement = complementElement.querySelector('span.expandable__fixed__header__text__subtitle.font-1.text-grey')
-              // Verifique se há um elemento com a classe específica diretamente dentro do complementElement
-              let requiredElement = complementElement.querySelector('span.expandable__fixed__header__text__required.font-0.ml-2.text-primary')
-
-              //Text Content dos complementos
-              let titleComplement = complementTitleElement ? complementTitleElement.textContent : "";
-              let typeComplement = typeComplementElement ? typeComplementElement.textContent: "";
-              let required = requiredElement ? requiredElement.textContent: "";
-              
-              //Opções do complemento
-
-              let optionsElement = options = complementExpandable.querySelectorAll('.chooser-info__details.no-imagine-no-spacing[data-v-7dd5c793]')
-              optionsElement.forEach((optionElement) => {
-                optionTitleElement = optionElement.
-              }
-             );
-            
-              let complementData = {
-                titleComplement,
-                typeComplement,
-                required,
-              };
-
-              // Adicione o objeto ao dicionário complementsDict
-              complementsDict[complementName] = complementData;
-            // Adicione o dicionário complementsDict ao array de dados raspados
-            scrapedData.push(complementsDict);
-          })
-        });
-
-          // Crie um objeto com os dados do produto
-          let productData = {
-            title,
-            priceNow,
-            imgSrc,
-            descricao,
-            complementsDict
+        productCards.forEach((productCard) => {
+          let innerDiv = productCard.querySelector('div.item-card-container.row.justify-between');
+          if (innerDiv) {
+            innerDiv.click();
           }
 
-          console.log(productData);
-          // Adicione o objeto ao array de dados extraídos
-          scrapedData.push(productData);
+          setTimeout(() => {
+            let titleElement = document.querySelector('span.font-5');
+            let priceNowElement = document.querySelector('span.price__now.font-3');
+            let imgElement = document.querySelector('img');
+            let descricaoElement = document.querySelector('span.weight-400');
 
-          window.history.go(-1);
-          // Volte para a página anterior (a página da categoria) antes de continuar
-        }, 3000);
-        window.history.go(-1);
-      });
-    }, 3000); // Espere 3 segundos antes de extrair dados da página individual (ajuste conforme necessário)
-  });
-  // Retorne os dados extraídos como um JSON
-  return scrapedData;
+            let title = titleElement ? titleElement.textContent : "";
+            let priceNow = priceNowElement ? priceNowElement.textContent : "";
+            let imgSrc = imgElement ? imgElement.src : "";
+            let descricao = descricaoElement ? descricaoElement.textContent : "";
+
+            let complementExpandables = document.querySelectorAll('div.expandable');
+            complementExpandables.forEach((complementExpandable) => {
+              let complementElements = complementExpandable.querySelectorAll('div.expandable__fixed.py-2.px-4.pointer.bg-grey-12');
+
+              complementElements.forEach((complementElement) => {
+                let complementTitleElement = complementElement.querySelector('.expandable__fixed__header__text__title.font-2.text-black');
+                let typeComplementElement = complementElement.querySelector('span.expandable__fixed__header__text__subtitle.font-1.text-grey');
+                let requiredElement = complementElement.querySelector('span.expandable__fixed__header__text__required.font-0.ml-2.text-primary');
+
+                let titleComplement = complementTitleElement ? complementTitleElement.textContent : "";
+                let typeComplement = typeComplementElement ? typeComplementElement.textContent : "";
+                let required = requiredElement ? requiredElement.textContent : "";
+
+                let optionsElement = complementExpandable.querySelectorAll('.chooser-info__details.no-imagine-no-spacing[data-v-7dd5c793]')
+                optionsElement.forEach((optionElement) => {
+                  let optionTitleElement = optionElement.querySelector('span.weight-700.text-black.font-1.mb-1');
+                  let optionPriceElement = optionElement.querySelector('div.chooser:nth-of-type(1) span.price__now');
+                  let optionQtdElement = optionElement.querySelector('div.chooser:nth-of-type(1) span.mt-1');
+
+                  let optionTitle = optionTitleElement ? optionTitleElement.textContent : "";
+                  let optionPrice = optionPriceElement ? optionPriceElement.textContent : "0";
+                  let optionQtd = optionQtdElement ? optionQtdElement.textContent : "";
+
+                  this.optionsComplement.push({ optionTitle, optionPrice, optionQtd });
+                });
+                
+                this.complementsDict[complementName] = {
+                  titleComplement,
+                  typeComplement,
+                  required,
+                  optionsComplement: this.optionsComplement,
+                };
+              });
+            });
+
+            let productData = {
+              title,
+              priceNow,
+              imgSrc,
+              descricao,
+              complementsDict: this.complementsDict
+            }
+
+            // Armazena os dados no dicionário com a chave categoryTitle
+            this.scrapedData[categoryTitle] = productData;
+            console.log(this.scrapedData);
+          }, 100);
+        });
+      }, 3000);
+    });
+  }
 }
 
 // Execute a função quando a página estiver totalmente carregada
 window.addEventListener('load', () => {
-  const jsonData = scrapeData();
-  console.log(jsonData); // Exibe o JSON no console
+  const scraper = new Scrapy();
+  scraper.checkAndScrape();
+  
+  // Transforma this.scrapedData em JSON e exibe no console
+  const jsonData = JSON.stringify(scraper.scrapedData);
+  console.log(jsonData);
 });
--- popup.js
+++ popup.js
@@ -1,33 +1,34 @@
-// Função para lidar com o clique no botão
-document.getElementById('scrape-button').addEventListener('click', () => {
-  // Execute um script na guia ativa para injetar e chamar a função scrapeData
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs[0]) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        files: ['content.js'] // Injeta o content.js na página ativa
-      }, () => {
-        // Após a injeção do script, chame a função scrapeData
+  // Função para lidar com o clique no botão
+  document.getElementById('scrape-button').addEventListener('click', () => {
+    // Execute um script na guia ativa para injetar e chamar a função checkAndScrape
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]) {
         chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
-          function: () => {
-            // Chame a função scrapeData definida no content.js
-            return scrapeData();
-          }
-        }, (results) => {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
-          } else {
-            try {
-              // Os resultados conterão o valor retornado pela função scrapeData
-              const scrapedData = results[0];
-              console.log(scrapedData); // Exibe os dados no console
-            } catch (error) {
-              console.error('Erro ao processar os dados:', error);
+          files: ['content.js'] // Injeta o content.js na página ativa
+        }, () => {
+          // Após a injeção do script, chame a função checkAndScrape
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: () => {
+              // Chame a função checkAndScrape definida no content.js
+              const scraper = new Scrapy();
+              return scraper.checkAndScrape();
             }
-          }
+          }, (results) => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError);
+            } else {
+              try {
+                // Os resultados conterão o valor retornado pela função checkAndScrape
+                const scrapedData = results[0];
+                console.log(scrapedData); // Exibe os dados no console
+              } catch (error) {
+                console.error('Erro ao processar os dados:', error);
+              }
+            }
+          });
         });
-      });
-    }
+      }
+    });
   });
-});
