e0eb597586ea32272a4fd493ab64a39329aa0d97
kayantchian <kayan.tchian@cardapioweb.com>
2023-10-10 16:56:41 -0300

    csv function convertion

--- content.js
+++ content.js
@@ -26,99 +26,113 @@ class Scrapy {
   
 
   async clickProductCards() {
+    await sleep(500)
     let categoryDivs = document.querySelectorAll('.category-container[data-v-c24acdb4]');
-
-    for await (const categoryDiv of categoryDivs) {
+  
+    for await (const categoryIndex of [...Array(categoryDivs.length).keys()]) {
+      await sleep(500)
+      let categoryDivs = document.querySelectorAll('.category-container[data-v-c24acdb4]');
+      let categoryDiv = categoryDivs[categoryIndex];
       let categoryNameElement = categoryDiv.querySelector('span[data-v-c24acdb4]');
       let categoryName = categoryNameElement ? categoryNameElement.textContent : "";
-
-        let productCards = categoryDiv.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small[data-v-58044642]');
-
-        for await (const productCard of productCards){
-        let productData = [];
-          let innerDiv = productCard.querySelector('div.item-card-container.row.justify-between');
-          if (innerDiv) {
-            innerDiv.click();
-
-
-            // Agora, vamos adicionar um atraso antes de coletar os dados.
-              await this.this.sleep(1500)
-              let titleElement = document.querySelector('span.font-5');
-              let priceNowElement = document.querySelector('span.price__now.font-3');
-              let imgElement = document.querySelector('img');
-              let descricaoElement = document.querySelector('span.weight-400');
-
-              let title = titleElement ? titleElement.textContent : "";
-              let priceNow = priceNowElement ? priceNowElement.textContent : "";
-              let imgSrc = imgElement ? imgElement.src : "";
-              let descricao = descricaoElement ? descricaoElement.textContent : "";
-
-              let complementsDict = []
-              let complementExpandables = document.querySelectorAll('div.expandable');
-              for await (const complementExpandable of complementExpandables) {
-                let complementElements = complementExpandable.querySelectorAll('div.expandable__fixed.py-2.px-4.pointer.bg-grey-12');
-                let optionsComplement = [];
-
-                // Pegar o nome de cada complemento
-                for await (const complementElement of complementElements){
-                  let typeComplementElement = complementElement.querySelector('span.expandable__fixed__header__text__subtitle.font-1.text-grey');
-                  let requiredElement = complementElement.querySelector('span.expandable__fixed__header__text__required.font-0.ml-2.text-primary');
-                  let complementNameElement = complementElement.querySelector('span.expandable__fixed__header__text__title');
-
-                  let typeComplement = typeComplementElement ? typeComplementElement.textContent : "";
-                  let required = requiredElement ? requiredElement.textContent : "";
-                  let complementName = complementNameElement ? complementNameElement.textContent : "";
-
-                  // Pegar nome de cada opção do complemento da iteração
-                  let optionsElement = complementExpandable.querySelectorAll('.chooser');
-                  for await (const optionElement of optionsElement) {
-                    await this.this.sleep(200)
-                    let optionTitleElement = optionElement.querySelector('span.weight-700.text-black.font-1.mb-1');
-                    let optionPriceElement = optionElement.querySelector('div.chooser:nth-of-type(1) span.price__now');
-                    let optionQtdElement = optionElement.querySelector('span.text-grey-3');
-                    let optionDescriptionElement = optionElement.querySelector('.chooser-info__description');
-
-                    let optionTitle = optionTitleElement ? optionTitleElement.textContent : "";
-                    let optionPrice = optionPriceElement ? optionPriceElement.textContent : "0";
-                    let optionQtd = optionQtdElement ? optionQtdElement.textContent : "";
-                    let optionDescription = optionDescriptionElement ? optionDescriptionElement.textContent : "";
-
-                    optionsComplement.push({
-                      optionTitle: optionTitle,
-                      optionPrice: optionPrice,
-                      optionQtd: optionQtd,
-                      optionDescription: optionDescription
-                    });
-                  }
-
-                  complementsDict.push({
-                    nameComplement: complementName,
-                    typeComplement: typeComplement,
-                    required: required,
-                    options: optionsComplement,
-                  })
-                }
+  
+      let productCards = categoryDiv.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small');
+  
+      let productData = [];
+      for await (const productIndex of [...Array(productCards.length).keys()]) {
+        await sleep(500)
+        let categoryDivs = document.querySelectorAll('.category-container[data-v-c24acdb4]');
+        let categoryDiv = categoryDivs[categoryIndex];
+        let productCards = categoryDiv.querySelectorAll('.item-card.col-8.category-container__products__product-list__item-card.not-small');
+        let productCard = productCards[productIndex];
+  
+        console.log(productCard.textContent); //debuging..
+  
+        let innerDiv = productCard.querySelector('.item-card-container.row.justify-between');
+        if (innerDiv) {
+          await sleep(500)
+          innerDiv.click();
+          console.log("clicou")
+          // Agora, vamos adicionar um atraso antes de coletar os dados.
+          await sleep(1000)
+  
+          let titleElement = document.querySelector('span.font-5');
+          let priceNowElement = document.querySelector('span.price__now.font-3');
+          let imgElement = document.querySelector('img');
+          let descricaoElement = document.querySelector('span.weight-400');
+          console.log("extraindo dados")
+          let title = titleElement ? titleElement.textContent : "";
+          let priceNow = priceNowElement ? priceNowElement.textContent : "";
+          let imgSrc = imgElement ? imgElement.src : "";
+          let descricao = descricaoElement ? descricaoElement.textContent : "";
+  
+          let complementsDict = []
+          let complementExpandables = document.querySelectorAll('div.expandable');
+          for await (const complementExpandable of complementExpandables) {
+            let complementElements = complementExpandable.querySelectorAll('div.expandable__fixed.py-2.px-4.pointer.bg-grey-12');
+            let optionsComplement = [];
+  
+            // Pegar o nome de cada complemento
+            for await (const complementElement of complementElements) {
+              let typeComplementElement = complementElement.querySelector('span.expandable__fixed__header__text__subtitle.font-1.text-grey');
+              let requiredElement = complementElement.querySelector('span.expandable__fixed__header__text__required.font-0.ml-2.text-primary');
+              let complementNameElement = complementElement.querySelector('span.expandable__fixed__header__text__title');
+  
+              let typeComplement = typeComplementElement ? typeComplementElement.textContent : "";
+              let required = requiredElement ? requiredElement.textContent : "";
+              let complementName = complementNameElement ? complementNameElement.textContent : "";
+  
+              // Pegar nome de cada opção do complemento da iteração
+              let optionsElement = complementExpandable.querySelectorAll('.chooser');
+              for await (const optionElement of optionsElement) {
+                let optionTitleElement = optionElement.querySelector('span.weight-700.text-black.font-1.mb-1');
+                let optionPriceElement = optionElement.querySelector('div.chooser:nth-of-type(1) span.price__now');
+                let optionQtdElement = optionElement.querySelector('span.text-grey-3');
+                let optionDescriptionElement = optionElement.querySelector('.chooser-info__description');
+  
+                let optionTitle = optionTitleElement ? optionTitleElement.textContent : "";
+                let optionPrice = optionPriceElement ? optionPriceElement.textContent : "0";
+                let optionQtd = optionQtdElement ? optionQtdElement.textContent : "";
+                let optionDescription = optionDescriptionElement ? optionDescriptionElement.textContent : "";
+  
+                optionsComplement.push({
+                  optionTitle: optionTitle,
+                  optionPrice: optionPrice,
+                  optionQtd: optionQtd,
+                  optionDescription: optionDescription
+                });
               }
-
-              productData.push({
-                title: title,
-                priceNow: priceNow,
-                imgSrc: imgSrc,
-                descricao: descricao,
-                complementsDict: complementsDict
-              });
+              console.log("options extraida")
+  
+              complementsDict.push({
+                nameComplement: complementName,
+                typeComplement: typeComplement,
+                required: required,
+                options: optionsComplement,
+              })
             }
-              else{
-                console.log("nao cli")
-              }
-            }
-              this.scrapedData.push({
-                categoryName: categoryName,
-                productsCategory: productData
-              });
-              await backPage();
+            console.log("complements extraida")
+          }
+  
+          productData.push({
+            title: title,
+            priceNow: priceNow,
+            imgSrc: imgSrc,
+            descricao: descricao,
+            complementsDict: complementsDict
+          });
+          console.log("productData extraido")
+          await backPage();
+        }
+      }
+      this.scrapedData.push({
+        categoryName: categoryName,
+        productsCategory: productData
+      });
+      console.log("scrapedData adicionado")
+      await backPage();
     }
-  }
+}
 }
 
 async function backPage() {
